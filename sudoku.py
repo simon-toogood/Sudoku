36d810254ba2075b2eac1c97c28eccc131fe35b7
deflatedfruit <66580940+deflated-fruit@users.noreply.github.com>
2020-06-09 14:23:22 +0100

    Cleaned up code and added docstrings

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+import functools
 import random
 
 
 WHITE = "#ffffff"
 RED = "#ffb5b5"
 ORANGE = "#ffdeb5"
-YELLOW = "#fff980"
+YELLOW = "#fffca1"
 GREEN = "#b7ffb5"
 BLUE = "#b0fff4"
 PURPLE = "#d4b0ff"
@@ -20,9 +21,13 @@ COLOUR = "colour"
 MODES = [NORMAL, CORNER, CENTRE, COLOUR]
 
 
+@functools.total_ordering
 class Cell(tk.Canvas):
-    def __init__(self, master, position, fixed=None, size=50, bg="white", **kwargs):
-        super().__init__(master, width=size, height=size, bg=bg, highlightthickness=0, **kwargs)
+    """A single Sudoku cell. Supports Snyder (corner) notes, centre notes and cell colouring. Can be compared against
+    other cells and integers (for example in pseudocode: Cell() == Cell(5) and Cell() == 5 are both supported. A Cell
+    can be fixed (value cannot be changed by the user) or it can be changed"""
+    def __init__(self, master, position, fixed=None, size=50, bg=WHITE):
+        super().__init__(master, width=size, height=size, bg=bg, highlightthickness=0)
         self.position = position
         self.size = size
         self.colour = bg
@@ -46,14 +51,27 @@ class Cell(tk.Canvas):
     def __repr__(self):
         return f"<Cell: n={self.number}>"
 
-    def add_corner_note(self, event):
-        if event.keysym == "BackSpace":
-            self.cornernotes = []
-            self._refresh()
-            return
-        num = event.char
-        if num not in "123456789":
-            return
+    def __eq__(self, other):
+        """Compare the number in this Cell against the number in another Cell or an integer"""
+        if isinstance(other, Cell):
+            return self.number == other.number
+        elif isinstance(other, int):
+            return self.number == other
+        else:
+            print(type(other))
+            raise NotImplementedError
+
+    def __lt__(self, other):
+        """Compare the number in this Cell against the number in another Cell or an integer"""
+        if isinstance(other, Cell):
+            return self.number < other.number
+        elif isinstance(other, int):
+            return self.number < other
+        else:
+            raise NotImplementedError
+
+    def add_corner_note(self, num):
+        """Add the given integer to the corner notes of the Cell"""
         if num in self.cornernotes:
             self.remove_corner_note(num)
         elif len(self.cornernotes) == 8:
@@ -63,17 +81,12 @@ class Cell(tk.Canvas):
         self._refresh()
 
     def remove_corner_note(self, num):
+        """Remove the given integer from the corner notes of the Cell"""
         self.cornernotes.remove(num)
         self._refresh()
 
-    def add_centre_note(self, event):
-        if event.keysym == "BackSpace":
-            self.cornernotes = []
-            self._refresh()
-            return
-        num = event.char
-        if num not in "123456789":
-            return
+    def add_centre_note(self, num):
+        """Add the given integer to the centre notes of the Cell"""
         if num in self.centrenotes:
             self.remove_centre_note(num)
         else:
@@ -81,31 +94,46 @@ class Cell(tk.Canvas):
         self._refresh()
 
     def remove_centre_note(self, num):
+        """Remove the given integer from the centre notes of the Cell"""
         self.centrenotes.remove(num)
         self._refresh()
 
-    def set_num(self, event):
-        num = event.char
-        if self.number == num or event.keysym == "BackSpace":
-            self.number = None
-        elif num in "123456789":
-            self.number = num
+    def set_num(self, num):
+        """Set the Cell to be this number, if it is not fixed"""
+        if not self.fixed:
+            if self.number == num:
+                self.number = None
+            else:
+                self.number = num
         self._refresh()
 
-    def set_colour(self, event):
+    def set_colour(self, colour):
+        """Set the Cell background to be the colour supplied"""
+        self.colour = colour
+        self._refresh()
+
+    def _translate(self, event):
+        """Translate a tkinter keyboard Event object to a call to the action desired by the user"""
         if event.keysym == "BackSpace":
-            self.colour = WHITE
+            self.cornernotes = []
+            self.centrenotes = []
+            self.set_num(None)
             self._refresh()
+        elif event.char not in "123456789":
             return
-        if event.char in "123456789":
-            self.colour = COLOURS[int(event.char) - 1]
-        self._refresh()
-
-    def set_mode(self, mode):
-        self.mode = mode
-        self._set_binds()
+        else:
+            num = int(event.char)
+            if self.mode == NORMAL:
+                self.set_num(num)
+            elif self.mode == CORNER:
+                self.add_corner_note(num)
+            elif self.mode == CENTRE:
+                self.add_centre_note(num)
+            elif self.mode == COLOUR:
+                self.set_colour(COLOURS[int(num) - 1])
 
     def _refresh(self):
+        """Refresh the Cell to update the Cell on screen to display changes to the notes, colour and number"""
         self.delete(tk.ALL)
         if self.number is not None:
             if not self.fixed:
@@ -119,27 +147,19 @@ class Cell(tk.Canvas):
             self.create_text(*self.centre, text="".join(map(str, self.centrenotes)))
 
     def _focusin(self, _):
-        self.config(bg="#fffcb5")
-        self._set_binds()
+        """When the Cell has focus, highlight it and allow it to take keyboard input"""
+        self.config(bg=average_colours("#fffcb5", self.colour))
+        self.bind("<Key>", self._translate)
 
     def _focusout(self, _):
+        """When the Cell loses focus, revert the colour to the original and disable keyboard input to the Cell"""
         self.config(bg=self.colour)
         self.unbind("<Key>")
 
-    def _set_binds(self):
-        self.unbind("<Key>")
-        if not self.fixed:
-            if self.mode == NORMAL:
-                self.bind("<Key>", self.set_num)
-            elif self.mode == CORNER:
-                self.bind("<Key>", self.add_corner_note)
-            elif self.mode == CENTRE:
-                self.bind("<Key>", self.add_centre_note)
-        if self.mode == COLOUR:
-            self.bind("<Key>", self.set_colour)
-
 
 class Sudoku(tk.Frame):
+    """A class that contains a 9x9 grid of Cells for a classic Sudoku. Contains methods for getting regions of the grid 
+    and checking for duplicates"""
     def __init__(self, master, cellsize=50, fixednumbers=None, **kwargs):
         super().__init__(master, **kwargs)
         self.cellsize = cellsize
@@ -167,12 +187,16 @@ class Sudoku(tk.Frame):
         self.mode.set(NORMAL)
         self.mode.trace("w", lambda *a: self.change_mode(self.mode.get()))
         self.modebuttons = [tk.Radiobutton(self, variable=self.mode, value=MODES[n], text=MODES[n].title()) for n in range(4)]
+        self.checkbutton = tk.Button(self, text="Check", command=lambda: print(self.check()))
         self.cellFrame.grid(row=0, column=1, rowspan=99)
         for i, m in enumerate(self.modebuttons):
             m.grid(row=i, column=2)
+        self.checkbutton.grid(row=4, column=2)
         self.master.bind("<Key>", self.move_focus)
 
     def load(self, grid):
+        """Load a Sudoku grid from a 2D list of numbers (None for an empty cell). The cell numbers loaded here are
+        fixed, meaning they cannot be changed by the user"""
         for x, row in enumerate(grid):
             for y, num in enumerate(row):
                 if num is not None:
@@ -182,21 +206,40 @@ class Sudoku(tk.Frame):
                     c._refresh()
 
     def get_row(self, row):
+        """Get row n of the grid. Note that it starts at 0 on the left"""
         return self.cells[row]
 
     def get_column(self, col):
+        """Get column n of the grid. Note that it starts at 0 on the left"""
         return [row[col] for row in self.cells]
 
     def get_box(self, box):
-        # what's a good way of doing this then?
-        pass
+        """Get box n of the grid. Note that it starts at 0 in the top-left and down to 8 in the bottom right"""
+        trans = [[0,1,2], [3,4,5], [6,7,8]]
+        rows, columns = trans[box%3], trans[box//3]
+        out = []
+        for r in rows:
+            for c in columns:
+                out.append(self.cells[r][c])
+        return out
+
+    def check(self):
+        """Check for and highlight any duplicate cells in regions"""
+        for n in range(9):
+            regions = [self.get_row(n), self.get_column(n), self.get_box(n)]
+            for r in regions:
+                if check_duplicates(r):
+                    return False
+        return True
 
     def change_mode(self, mode):
+        """Change the global input mode for the grid"""
         for row in self.cells:
             for cell in row:
-                cell.set_mode(mode)
+                cell.mode = mode
 
     def move_focus(self, event):
+        """Use the arrow keys to move focus around the grid"""
         pos = self.focus_get().position
         key = event.keysym
         if key == "Up":
@@ -207,8 +250,23 @@ class Sudoku(tk.Frame):
             pos = pos[0], (pos[1] - 1) % 9
         elif key == "Right":
             pos = pos[0], (pos[1] + 1) % 9
+            print(self.get_box(0))
         self.cells[pos[0]][pos[1]].focus_set()
 
+
+def average_colours(c1, c2):
+    """Takes the geometric average of two hex colours."""
+    def hextorgb(h):
+        return tuple(int(h[i:i + 2], 16) / 255. for i in (1, 3, 5))
+    def rgbtohex(rgb):
+        return f'#{int(rgb[0]*255):02x}{int(rgb[1]*255):02x}{int(rgb[2]*255):02x}'
+    rgb = (hextorgb(c1), hextorgb(c2))
+    return rgbtohex([sum(y) / len(y) for y in zip(*rgb)])
+
+
+def check_duplicates(lst):
+    """Check for duplicate values in a list. Returns a list of duplicates, or an empty list if there are none"""
+    lst.sort()
 
 if __name__ == "__main__":
     root = tk.Tk()
